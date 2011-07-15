83f0a1d4bf680deea998e7bf27b06d4f3be5672f
El-Flaco <el.flaco.fede@gmail.com>
2011-07-15 16:57:20 -0300

    comenté la interfaz

--- src/MiArbol.java
+++ src/MiArbol.java
@@ -12,16 +12,16 @@ import java.util.Iterator;
  * @author ThElFlacox
  */
 public interface MiArbol <E> extends Collection <E> {
-    boolean hijo (E e);
-    E hijoDer (E e);
-    E hijoIzq (E e);
+    boolean hijo (E e);//Devuelve verdader si el nodo tiene un hijo
+    E hijoDer (E e);//Devuelve el hijo derecho del nodo
+    E hijoIzq (E e);//Devuelve el hijo izquierdo del nodo
     Iterator <E> hojas();
-    int gradoNodo (E e);
-    int gradoArbol();
+    int gradoNodo (E e);//Devuelve el grado del nodo
+    int gradoArbol();//Devuelve el grado del árbol
     MiArbol subArbolDe(E e);
     MiArbol subArbolIz(E e);
     Iterator <E> preOrden();
     Iterator <E> inOrden();
     Iterator <E> posOrden();
-    E nodoRaiz();
+    E nodoRaiz();//Devuelve el nodo raíz del árbol
 }
